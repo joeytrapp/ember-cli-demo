0d0c9db7765bc06dea738bdfc3d893238540ae66
Joey Trapp <joey@loadsys.com>
2014-09-10 12:19:19 -0500

    Brian added ways to transfer bootstrap classes to button and I refactored

--- app/components/ls-confirm-button.js
+++ app/components/ls-confirm-button.js
@@ -5,8 +5,19 @@ export default Ember.Component.extend({
   attributeBindings: ['type'],
   type: 'button',
   confirmText: 'Are you sure?',
+  classNameBindings: ['classes'],
 
-  click: function(e) {
+  classes: function() {
+    var requested = this.get('style'), extra = ['btn-default'];
+    if (typeof requested === 'string') {
+      extra = requested.split(' ').map(function(i) {
+        return 'btn-' + i;
+      });
+    }
+    return ['btn'].concat(extra).join(' ');
+  }.property('style'),
+
+  click: function() {
     if (confirm(this.get('confirmText'))) {
       this.sendAction();
     }
